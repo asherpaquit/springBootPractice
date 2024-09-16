93a1d66ecf93142f9ef46c66e1e4eae551fb667f
paxx <asherpoquit@gmail.com>
2024-09-16 23:20:56 +0800

    fixed

--- francis_and_friends/src/main/java/com/g1/appdev/francis_and_friends/StudentSimplifyApplication.java
+++ francis_and_friends/src/main/java/com/g1/appdev/francis_and_friends/StudentSimplifyApplication.java
@@ -1,10 +1,14 @@
 package com.g1.appdev.francis_and_friends;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentSimplifyApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(StudentSimplifyApplication.class);
 
 	public static void main(String[] args)
 	{
--- francis_and_friends/src/main/java/com/g1/appdev/francis_and_friends/project/ProjectDetailsController.java
+++ francis_and_friends/src/main/java/com/g1/appdev/francis_and_friends/project/ProjectDetailsController.java
@@ -3,18 +3,24 @@ package com.g1.appdev.francis_and_friends.project;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/runs")
+@RequestMapping("/wow")
 public class ProjectDetailsController {
     private final ProjectDetailsRepository projectDetailsRepository;
 
     public ProjectDetailsController(ProjectDetailsRepository projectDetailsRepository){
         this.projectDetailsRepository = projectDetailsRepository;
     }
+    @GetMapping("findAll")
+    List<ProjectDetails> findall(){
+        return projectDetailsRepository.findAll();
+    }
+
     // Reading part
-    @GetMapping("/{id}")
+    @GetMapping("/{projectId}")
     ProjectDetails findById(@PathVariable Integer projectId){
         Optional<ProjectDetails> proDe = projectDetailsRepository.findById(projectId);
         if(proDe.isEmpty()){
@@ -25,20 +31,20 @@ public class ProjectDetailsController {
 
     // Post
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("")
+    @PostMapping("add")
     void create(@RequestBody ProjectDetails proDe){
         projectDetailsRepository.create(proDe);
     }
 
     // Put
     @ResponseStatus(HttpStatus.CREATED)
-    @PutMapping("/{id}")
+    @PutMapping("/{projectId}")
     void update(@RequestBody ProjectDetails proDe, @PathVariable Integer projectId){
         projectDetailsRepository.update(proDe,projectId);
     }
 
     // Delete
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{projectId}")
     void delete(@PathVariable Integer projectId){
         projectDetailsRepository.delete(projectId);
     }
